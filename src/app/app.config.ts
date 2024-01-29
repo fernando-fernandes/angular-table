a2281f1e19befb3691a29e7a38bbd68d2e3861d1
Fernando Fernandes <fernando.costa@kumon.com.br>
2024-01-29 16:05:25 -0300

    base front

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -5,6 +5,7 @@ import { routes } from './app.routes'
 import { provideAnimations } from '@angular/platform-browser/animations'
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async'
 
+
 export const appConfig: ApplicationConfig = {
   providers: [
     provideRouter(routes),
--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,29 +1,34 @@
 import { Component, OnInit } from '@angular/core'
 import { HeaderComponent } from '../../shared/header/header.component'
 import { CalendarModule } from 'primeng/calendar'
-import { DividerModule } from 'primeng/divider'
 import { DropdownModule } from 'primeng/dropdown'
 import { TableModule } from 'primeng/table'
 import { FormsModule } from '@angular/forms'
 import { CommonModule } from '@angular/common'
 import { ApiService } from '../../services/api.service'
+import { TagModule } from 'primeng/tag'
+import { ButtonModule } from 'primeng/button'
+import { MessageService, SelectItem } from 'primeng/api'
+import { ToastModule } from 'primeng/toast'
+import { InputTextModule } from 'primeng/inputtext'
+
+
 
 interface City {
   name: string
   code: string
 }
-
 interface Product {
-  id?: string
-  code?: string
-  name?: string
-  description?: string
-  price?: number
-  quantity?: number
-  inventoryStatus?: string
-  category?: string
-  image?: string
-  rating?: number
+  id: string,
+  nomeAluno: string,
+  serieEscolar: string,
+  nomeResponsavel: string,
+  celularResponsavel: string,
+  emailResponsavel: string,
+  dadosAtualizados: string,
+  dataAtualizacao: string,
+  atualizadoPor: string,
+  statusIt: string,
 }
 
 @Component({
@@ -36,23 +41,39 @@ interface Product {
     CommonModule,
     HeaderComponent,
     CalendarModule,
-    DividerModule,
     DropdownModule,
-    TableModule
+    TableModule,
+    TagModule,
+    ButtonModule,
+    ToastModule,
+    InputTextModule
   ],
+  providers: [MessageService]
 })
 export class HomeComponent implements OnInit {
+
   countries: any[] | undefined
   cities: City[] | undefined
-
   selectedCity: City | undefined
-
   products!: Product[]
+  statuses!: SelectItem[]
+  clonedProducts: { [s: string]: Product } = {};
 
-  constructor(private productService: ApiService) { }
+  constructor(private productService: ApiService, private messageService: MessageService) { }
 
 
   ngOnInit() {
+
+
+    this.productService.getProductsMini().then((data) => {
+      this.products = data
+    })
+
+    this.statuses = [
+      { label: 'Sim', value: '1' },
+      { label: 'Não', value: '2' }
+    ]
+
     this.cities = [
       { name: 'New York', code: 'NY' },
       { name: 'Rome', code: 'RM' },
@@ -60,9 +81,25 @@ export class HomeComponent implements OnInit {
       { name: 'Istanbul', code: 'IST' },
       { name: 'Paris', code: 'PRS' }
     ]
+  }
 
-    this.productService.getProductsMini().then((data) => {
-      this.products = data
-    })
+  onRowEditInit(product: Product) {
+    this.clonedProducts[product.id as string] = { ...product }
+  }
+
+  onRowEditSave(product: Product) {
+    // if (product.price > 0) {
+    //   delete this.clonedProducts[product.id as string]
+    //   this.messageService.add({ severity: 'success', summary: 'Success', detail: 'Product is updated' })
+    // } else {
+    //   this.messageService.add({ severity: 'error', summary: 'Error', detail: 'Invalid Price' })
+    // }
+
+    this.messageService.add({ severity: 'success', summary: 'Sucesso', detail: 'Os dados do aluno foram atualizados.' })
+  }
+
+  onRowEditCancel(product: Product, index: number) {
+    this.products[index] = this.clonedProducts[product.id as string]
+    delete this.clonedProducts[product.id as string]
   }
 }
